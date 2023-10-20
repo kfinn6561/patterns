47dbf1b36cc15c5248d65816db443426c9f90756
Kieran Finn <kieran.finn@skyscanner.net>
2023-10-20 17:30:07 +0100

    added director class

---
+++ src/main/java/org/example/builder/Director.java
@@ -0,0 +1,37 @@
+package org.example.builder;
+
+publc class Director{
+    publc IVehicleBuilder buildMercedes(IVehicleBuilder builder){
+        return builder
+        .setName("Mercedes")
+        .setDoors(5)
+        .setFuelType(FuelType.PETROL)
+        .setTankSize(100.0)
+        .setEngineSize(10.0)
+    }
+
+publc IVehicleBuilder buildTesla(IVehicleBuilder builder){
+        return builder
+        .setName("Tesla")
+        .setDoors(5)
+        .setFuelType(FuelType.ELECTRIC)
+        .setTankSize(0.0)
+        .setEngineSize(30.0)
+    }
+    publc IVehicleBuilder buildPrius(IVehicleBuilder builder){
+        return builder
+        .setName("Toyota Prius")
+        .setDoors(3)
+        .setFuelType(FuelType.HYBRID)
+        .setTankSize(30.0)
+        .setEngineSize(5.0)
+    }
+    publc IVehicleBuilder buildTransit(IVehicleBuilder builder){
+        return builder
+        .setName("Ford Transit van")
+        .setDoors(7)
+        .setFuelType(FuelType.DIESEL)
+        .setTankSize(200.0)
+        .setEngineSize(15.0)
+    }
+}
