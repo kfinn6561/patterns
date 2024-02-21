7022cabf6e4df6087b37f1b3306796fd022fc620
Kieran Finn <kieran.finn@skyscanner.net>
2024-02-21 15:39:33 +0000

    changed test to be multiple instead of just two

--- src/test/java/org/example/singleton/SingletonTest.java
+++ src/test/java/org/example/singleton/SingletonTest.java
@@ -2,15 +2,25 @@ package org.example.singleton;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class SingletonTest {
   @Test
   public void testCreateManySingletons() {
-    Singleton singletonA = Singleton.getInstance();
-    double expectedID = singletonA.getId();
+    // given
+    int numberOfSingletons = 100;
 
-    Singleton singletonB = Singleton.getInstance();
-    assertEquals(expectedID, singletonB.getId());
+    // when
+    List<Singleton> singletonList = new ArrayList<>(numberOfSingletons);
+    for (int i = 0; i < numberOfSingletons; i++) {
+      singletonList.add(Singleton.getInstance());
+    }
+
+    // then
+    for (int i = 1; i < numberOfSingletons; i++) {
+      assertEquals(singletonList.get(0).getId(), singletonList.get(i).getId());
+    }
   }
 }
