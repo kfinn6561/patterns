a80152109be2a5b0564ec0b1881847fed4d0bd0d
Kieran Finn <kieran.finn@skyscanner.net>
2025-05-14 16:58:59 +0100

    added comments

--- src/main/java/org/example/factory/GroundLogisticsHub.java
+++ src/main/java/org/example/factory/GroundLogisticsHub.java
@@ -1,6 +1,7 @@
 package org.example.factory;
 
 public class GroundLogisticsHub extends LogisticsHub {
+    // override the factory method to create a Truck
     @Override
     protected Transport createTransport(Location origin) {
         return new Truck(origin);
